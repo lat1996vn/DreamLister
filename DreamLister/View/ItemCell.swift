28ecba8b9125c0c832efdbd952dfe2f18ecf6d74
LTT <ltt@ltts-imac-5.local>
2018-08-06 10:58:11 +0700

    Created View Custom, Design App Interface

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by LTT on 8/6/18.
+//  Copyright © 2018 LTT. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var imgThumb: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    
+    
+}
---
+++ DreamLister/MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by LTT on 8/5/18.
+//  Copyright © 2018 LTT. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
