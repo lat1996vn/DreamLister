d621af6869b9cdbb85ce75108d1f7130c1f10418
LTT <ltt@ltts-imac-5.local>
2018-08-07 14:23:50 +0700

    connect story broad with code in ItemDetailsVC,  insert data into PickerView

--- DreamLister/Controller/ItemDetailVC.swift
+++ DreamLister/Controller/ItemDetailVC.swift
@@ -7,29 +7,63 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailVC: UIViewController {
-
+class ItemDetailVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var tfTitle: CustomTextField!
+    @IBOutlet weak var tfPrice: CustomTextField!
+    @IBOutlet weak var tfDetails: CustomTextField!
+    
+    var stores = [Store]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //update when select
+    }
+    
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest)
+            self.pickerView.reloadAllComponents()
+        } catch {
+            //handle the error
+        }
+    }
+    
+    func generateTestData() {
+        let store = Store(context: context)
+        store.name = "Best Buy"
+        let store2 = Store(context: context)
+        store2.name = "Tesla Dealership"
+        let store3 = Store(context: context)
+        store3.name = "Amazon"
+        let store4 = Store(context: context)
+        store4.name = "Frys Electronics"
+        let store5 = Store(context: context)
+        store5.name = "K Mart"
+        appDel.saveContext()
+    }
 }
